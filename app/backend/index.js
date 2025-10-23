bfd43175ee1830402a9921282bf9f90752e58073
Harshita Hirawat <harshitahirawat08@gmail.com>
2025-10-24 01:59:43 +0530

    Change CORS origin to production URL

--- app/backend/index.js
+++ app/backend/index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://bitebox-8gfl.onrender.com",
         credentials: true,
         methods: ['POST', 'GET']
     }
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000
 
 // middlewares
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://bitebox-8gfl.onrender.com",
     credentials: true
 }))
 app.use(express.json())
